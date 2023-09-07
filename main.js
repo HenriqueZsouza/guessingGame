60b9797c89d4b5adaa255f93d68d38f74c210784
Henrique <henriquezambon.s@gmail.com>
2023-09-07 15:52:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+// variáveis
+
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+const randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
+//Eventos
+
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+
+//Funções
+
+function handleTryClick(event) {
+  event.preventDefault()
+
+  const inputNumber = document.querySelector("#inputNumber")
+  
+  if(Number(inputNumber.value) == randomNumber) {
+    toggleScreen()
+    screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas`      
+  }
+
+  inputNumber.value = ""   
+  xAttempts++
+}
+
+function handleResetClick () {
+  screen1.classList.remove("hide")
+  screen2.classList.add("hide")
+  xAttempts = 1
+}
+
+function toggleScreen () {
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
+
+
+
+
+
+
